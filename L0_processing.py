f9786e8319fc2aa2ea9c0ec5c4da010e8a432d38
Jesus <eejvt@leeds.ac.uk>
2016-07-07 16:50:23 +0100

    L0 prepared to handle multiple timesteps in multiple ppfiles. It will extract every time step in every variable and then save it in a folder named with the timestep name. Need to prepare for concatenate files later on

--- L0_processing.py
+++ L0_processing.py
@@ -35,61 +35,44 @@ import variable_dict as vd
 reload(vd)
 
 
-orog_file = '/group_workspaces/jasmin2/gassp/jvergaratemprado/n96_hadgem1_qrparm.orog_new.pp'#jasmin
-orog_file = '/nfs/a107/earkpr/ACID-PRUFF/Masaru/OAT5/teafw/ppfiles/n96_hadgem1_qrparm.orog_new.pp'#leeds foe-linux
-#plt.interactive(0)
-#files_directory_UKCA='/nfs/a107/earkpr/DataVisualisation/UKCA/'
-files_directory_UKCA='/nfs/a201/'+username+'/UKCA_TEST_FILES/'
-run='tebxe/'
-files_directory=files_directory_UKCA+run
-pp_files=glob(files_directory+'*pp')
-date=pp_files[0][len(files_directory)+len(run)+3:-3]
 
-print date
-year=date[:4]
-print pp_files
+files_directory='/nfs/a201/eejvt/CASSIM/first_runs/SO/673/'
+pp_files=glob(files_directory+'umnsaa_*')
+# pp_files=[pp for pp in pp_files if not 'stash' in pp and not 'xhist' in pp]
 
 
-def from_pp_to_nc_single_var(step_file):
+def from_pp_to_nc_single_var_single_ts(step_file):
     print step_file
-    '''define things and create folders for nc (NETCDF) files'''
-    date=step_file[len(files_directory)+len(run)+3:-3]
-    print date
-    folder_NETCDF=files_directory+step_file[len(files_directory)+len(run)+3:-3]+'/'
-    ukl.create_folder(folder_NETCDF)
-
-    '''loading cubes from pp files and saving them in their correspondent folder'''
-    #cubes=iris.load(step_file)#long and heavy bit Time: around 15 minutes
-    cubes=iris.load([orog_file,step_file])#long and heavy bit. Time: around 15 minutes
+    cubes=iris.load(step_file)#long and heavy bit. Time: around 15 minutes
     for cube in cubes:
-       #capturing stash code from pp file
+        #capturing stash code from pp file
         stash_code=ukl.get_stash(cube)
         if stash_code in vd.variable_reference_stash:
             if not isinstance(cube.long_name,str):
                 cube.long_name=vd.variable_reference_stash[stash_code].long_name
                 print 'added long_name',cube.long_name, 'to', stash_code
-            if not isinstance(cube._var_name,str):
-                cube._var_name=vd.variable_reference_stash[stash_code].short_name
-                print 'added short_name as cube._var_name',cube._var_name, 'to', stash_code
-            #if not isinstance(cube.units,str):
-                #cube.units=vd.variable_reference_stash[stash_code].units
-                #print 'added units',cube.units, 'to', stash_code
-        #check that long name exists
-        if cube.long_name:
-            saving_name=folder_NETCDF+date+'_'+stash_code+'_'+cube.long_name+'.nc'
-        elif cube._var_name:
-            saving_name=folder_NETCDF+date+'_'+stash_code+'_'+cube._var_name+'.nc'
-        else:
-            saving_name=folder_NETCDF+date+'_'+stash_code+'.nc'
+                if not isinstance(cube._var_name,str):
+                    cube._var_name=vd.variable_reference_stash[stash_code].short_name
+                    print 'added short_name as cube._var_name',cube._var_name, 'to', stash_code
+        times=cube.coord('time').points
+        for it in range(len(times)):
+            cube_single_t=cube.extract(iris.Constraint(time=times[it]))
 
-        iris.save(cube,saving_name, netcdf_format="NETCDF4")
-        #cube=iris.load(saving_name)
+            folder_NETCDF=files_directory+str(int(times[it]))+'/'
+            ukl.create_folder(folder_NETCDF)
+
+            if cube._var_name:
+                saving_name=folder_NETCDF+str(int(times[it]))+'_'+stash_code+'_'+cube._var_name+'.nc'
+            else:
+                saving_name=folder_NETCDF+str(int(times[it]))+'_'+stash_code+'.nc'
+
+            iris.save(cube,saving_name, netcdf_format="NETCDF4")
 
 
 jobs=[]
 start=time.time()
 for step_file in pp_files:
-    p = multiprocessing.Process(target=from_pp_to_nc_single_var, args=(step_file,))
+    p = multiprocessing.Process(target=from_pp_to_nc_single_var_single_ts, args=(step_file,))
     jobs.append(p)
     p.start()
 
@@ -99,7 +82,7 @@ for job in jobs:
 end=time.time()
 
 print end-start
-
+'''
 #file_variable_name='2008apr_m01s00i101_mass_fraction_of_sulfur_dioxide_expressed_as_sulfur_in_air.nc'
 def join_variables(list_variables):
     for file_variable_name in list_variables:
@@ -174,3 +157,4 @@ for job in jobs:
     job.join()
 end=time.time()
 print end-start
+'''
