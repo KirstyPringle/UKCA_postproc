cb63adf2d4d1da045cb45f34045d9b5f159212b6
Jesus <eejvt@leeds.ac.uk>
2017-02-02 16:33:13 +0000

    some calculations added to L1_processing. Among those: LWP, IWP, CTT, CDNC, CINC, Mass of ice and water

--- L0_processing.py
+++ L0_processing.py
@@ -17,6 +17,8 @@ dir_scripts='/nfs/see-fs-01_users/eejvt/UKCA_postproc'#Change this to the downlo
 sys.path.append(dir_scripts)
 import UKCA_lib as ukl
 import numpy as np
+sys.path.append('/nfs/a107/eejvt/PYTHON_CODE')
+import Jesuslib as jl
 import time
 import iris
 from glob import glob
@@ -35,23 +37,35 @@ iris.FUTURE.netcdf_no_unlimited =False
 import variable_dict as vd
 reload(vd)
 from multiprocessing import Process, Manager
-
-files_directory='/nfs/a201/eejvt/CASIM/SO_KALLI/SECOND_DOMAIN/NOICE/'
-pp_files=glob(files_directory+'umnsaa_*')
+iris.FUTURE.netcdf_no_unlimited = True
+iris.FUTURE.netcdf_promote = True
+# files_directory='/nfs/a201/eejvt/UKCA_TEST_FILES/tebxd/'
+#files_directory='/nfs/a201/eejvt/CASIM/SO_KALLI/NO_INITIAL_ICE/3_ORD_LESS/'
+#files_directory='/nfs/a201/eejvt/CASIM/SO_KALLI/OLD_MICRO/'
+#files_directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/LARGE_DOMAIN/'
+files_directory='/nfs/a201/eejvt/CASIM/SO_KALLI/CLOUD_SQUEME/BASE/'
+#pp_files=glob(files_directory+'*.pp')
+pp_files=glob(files_directory+'umnsaa_p*')
 # pp_files=[pp for pp in pp_files if not 'stash' in pp and not 'xhist' in pp]
 manager = Manager()
 stashcodes=[]
 step_folders=[]
 stashcodes = manager.list()
 rotate_cube=True
+rotate_cube=False
 latlon0 = manager.list([-52.0,0.01])
 step_folders = manager.list()
+#%%
+#step_file=files_directory+'umnsaa_pc00173025'#'umnsaa_pc00215255'#'umnsaa_pc00225525'#'umnsaa_pc00015255'#'umnsaa_pc00000000'
+#from_pp_to_nc_single_var_single_ts(step_file)
 #%%
 def from_pp_to_nc_single_var_single_ts(step_file):
     #print step_file
     # global stashcodes
     # global step_folders
     cubes=iris.load(step_file)#long and heavy bit. Time: around 15 minutes
+    if len(cubes)==0:
+        return 'jumped'
     for cube in cubes:
         #capturing stash code from pp file
         stash_code=ukl.get_stash(cube)
@@ -70,13 +84,15 @@ def from_pp_to_nc_single_var_single_ts(step_file):
 
         if rotate_cube:
             if len(cube.coord('grid_longitude').points)==len(cube.coord('grid_latitude').points):
-                lons, lats =iris.analysis.cartography.unrotate_pole(cube.coord('grid_longitude').points,cube.coord('grid_latitude').points,latlon0[1],90+latlon0[0])
+                lons, lats =iris.analysis.cartography.unrotate_pole(cube.coord('grid_longitude').points,cube.coord('grid_latitude').points,latlon0[1],latlon0[0])
                 cube.coord('grid_longitude').points=lons
                 cube.coord('grid_latitude').points=lats
             else:
                 cube.coord('grid_latitude').points=cube.coord('grid_latitude').points+latlon0[0]
                 cube.coord('grid_longitude').points=cube.coord('grid_longitude').points+latlon0[1]+180
         for it in range(len(times)):
+            print times[it],step_file[len(files_directory):]
+        
             cube_single_t=cube.extract(iris.Constraint(time=times[it]))
 
             folder_NETCDF=files_directory+str(int(times[it]))+'/'
@@ -89,13 +105,23 @@ def from_pp_to_nc_single_var_single_ts(step_file):
                 saving_name=folder_NETCDF+str(int(times[it]))+'_'+stash_code+'_'+cube_single_t.long_name+'.nc'
             else:
                 saving_name=folder_NETCDF+str(int(times[it]))+'_'+stash_code+'.nc'
-
+            
             iris.save(cube_single_t,saving_name, netcdf_format="NETCDF4")
+            if cube_single_t.shape==(71,500,500):
+                print '--------------------------------------'
+                print '--------------------------------------'
+                print '--------------------------------------'
+                print '--------------------------------------'
+                print times[it],step_file[len(files_directory):]
+                print '--------------------------------------'
+                print '--------------------------------------'
+                print '--------------------------------------'
+                print '--------------------------------------'
 jobs=[]
 
 processes=20
 print 'Number of pp_files for L0', len(pp_files)
-list_of_chunks=np.array_split(pp_files,len(pp_files)/processes)
+list_of_chunks=np.array_split(pp_files,len(pp_files)/processes+1)
 start=time.time()
 for chunk in list_of_chunks:
     jobs=[]
@@ -131,10 +157,14 @@ def join_variables(list_variables):
             #print step_folder
             file_name=ukl.Obtain_name(step_folder,stash_code)
             if len(file_name)>=1:names.append(file_name[0])
+            names=list(set(names))
         cube_list=[]
+        print names
         #print len(names)
         #print names
         cube_list=iris.load(names)
+        print cube_list
+        print len(cube_list)
         if 'm01s00i033' == stash_code:
             print 'orography skipped'
             continue
@@ -188,3 +218,21 @@ for job in jobs:
     job.join()
 end=time.time()
 print end-start
+jl.send_email()
+#%%
+#
+#stash_code='m01s00i254'
+#names=[]
+#for step_folder in step_folders:
+#    # print files_directory
+#    #print step_folder
+#    file_name=ukl.Obtain_name(step_folder,stash_code)
+#    if len(file_name)>=1:names.append(file_name[0])
+#names=list(set(names))
+##%%
+#cube=iris.load(names)
+#for name in names:
+#    print name
+#    print iris.load(name)
+#
+#
--- L1_processing.py
+++ L1_processing.py
@@ -12,10 +12,12 @@ University of Leeds 2016
 
 """
 import sys
-dir_scripts='/nfs/see-fs-01_users/eejvt/svn_test/official'
+dir_scripts='/nfs/see-fs-01_users/eejvt/UKCA_postproc'
 sys.path.append(dir_scripts)#Change this to the downloaded folder
 import numpy as np
 import iris
+sys.path.append('/nfs/a107/eejvt/PYTHON_CODE')
+import Jesuslib as jl
 import UKCA_lib as ukl
 reload(ukl)
 import multiprocessing
@@ -23,14 +25,24 @@ import variable_dict as vd
 reload(vd)
 import I_MODE_SETUP_Variables as ims
 reload(ims)
+from matplotlib.colors import LogNorm
 
 def save_cube(cube):
     saving_name=saving_folder_l1+'L1_'+cube._var_name+'_'+cube.long_name+'.nc'
     iris.save(cube,saving_name, netcdf_format="NETCDF4")
     print 'saved:',cube.long_name
 
-directory='/nfs/a201/eejvt/UKCA_TEST_FILES/tebxd/'
-folder=directory+'All_months'
+#directory='/nfs/a201/eejvt/UKCA_TEST_FILES/tebxd/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/NO_INITIAL_ICE/BASE_RUN/'
+directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/ALL_ICE_PROC/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/3_ORD_LESS_762/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/SECOND_DOMAIN/NOICE/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/BASE_CONTACT_242/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/BASE_CONTACT_242/'
+#folder=directory+'All_time_steps/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/TRY2/2_ORD_MORE/'
+#directory='/nfs/a201/eejvt/CASIM/SO_KALLI/CLOUD_SQUEME/BASE/'
+folder=directory+'All_time_steps/'
 saving_folder_l1=directory+'L1/'
 ukl.create_folder(saving_folder_l1)
 
@@ -48,8 +60,13 @@ cp=1005.46 # J/kg/K
 Rd_cp=Rd/cp
 
 temperature=potential_temperature*(air_pressure/p0)**(Rd_cp)
+
+
 print temperature.data[0,0,0,0]
+temperature.long_name='Temperature'
 temperature._var_name='temperature'
+
+save_cube(temperature)
 R_specific=iris.coords.AuxCoord(287.058,
                           long_name='R_specific',
                           units='J-kilogram^-1-kelvin^-1')#J/(kg·K)
@@ -66,11 +83,126 @@ avogadro_number=iris.coords.AuxCoord(6.022e23,
 particle_density_of_air=air_density/molar_mass_air*avogadro_number
 
 print particle_density_of_air.data[0,0,0,0]
-
 air_density._var_name='air_density'
 air_density.long_name='Density of air'
 save_cube(air_density)
 
+cloud_number=iris.load(ukl.Obtain_name(folder,'m01s00i075_CLOUD_NUMBER_AFTER_TIMESTEP'))[0]
+
+CDNC=cloud_number*air_density
+CDNC.long_name='Cloud droplet number concentration'
+CDNC._var_name='CDNC'
+CDNC.units='meter^-3'
+
+save_cube(CDNC)
+
+cloud_ice_number=iris.load(ukl.Obtain_name(folder,'m01s00i271_CLOUD_ICE_(CRYSTALS)_AFTER_TIMESTEP'))[0]
+
+CINC=cloud_ice_number*air_density
+CINC.long_name='Cloud ice number concentration'
+CINC._var_name='CINC'
+CINC.units='meter^-3'
+save_cube(CINC)
+
+
+import copy
+#m01s15i101_height_above_reference_ellipsoid.nc
+height=iris.load(ukl.Obtain_name(folder,'m01s15i101'))[0]
+base=np.zeros(height.data.shape[1:])
+length_gridbox_cube=height.copy()#copy.deepcopy(height)
+length_gridbox=np.zeros(height.data.shape)
+#length_gridbox.data=np.zeros(length_gridbox.data.shape)
+#%%
+for i in range(height.data.shape[0]):
+    if i==0:
+        length_gridbox[0,].data=height[0,].data
+    else:
+        length_gridbox[i,]=height[i,].data-height[i-1,].data
+        print height[i,0,0].data
+        print height[i-1,0,0].data
+        print '---'
+        print height[i,0,0].data-height[i-1,0,0].data
+        print '---'
+
+#%%
+length_gridbox_cube.data=length_gridbox
+length_gridbox_cube.remove_coord('forecast_reference_time')
+length_gridbox_cube.remove_coord('forecast_period')
+#length_gridbox_cube.remove_coord('time')
+
+#length_gridbox_cube=iris.coords.AuxCoord(length_gridbox,
+#                          long_name='length_gridbox',
+#                          units='meter^1')#J/(kg·K)
+#%%
+
+stash_code='m01s00i254'#mass_fraction_of_cloud_liquid_water_in_air
+liquid_water_mmr=iris.load(ukl.Obtain_name(folder,stash_code))[0]
+liquid_water_mc=air_density*liquid_water_mmr
+liquid_water_mc.long_name='Mass concentration of cloud liquid water'
+liquid_water_mc._var_name='mcon_lw'
+save_cube(liquid_water_mc)
+LWP_column=np.empty(liquid_water_mc.data.shape[0]).tolist()
+
+for i in range(liquid_water_mc.data.shape[0]):
+    LWP_column[i]=(liquid_water_mc[i,]*length_gridbox_cube)
+
+LWP_cube_list=iris.cube.CubeList(LWP_column)
+LWP=LWP_cube_list.merge()[0]
+LWP=LWP.collapsed(['model_level_number'],iris.analysis.SUM)
+LWP._var_name='LWP'
+LWP.long_name='Liquid water path'
+save_cube(LWP)
+
+
+stash_code='m01s00i012'#mass_fraction_of_cloud_ice_in_air
+ice_water_mmr=iris.load(ukl.Obtain_name(folder,stash_code))[0]
+ice_water_mc=air_density*ice_water_mmr
+ice_water_mc.long_name='Mass concentration of cloud ice'
+ice_water_mc._var_name='mcon_iw'
+save_cube(ice_water_mc)
+IWP_column=np.empty(ice_water_mc.data.shape[0]).tolist()
+
+for i in range(ice_water_mc.data.shape[0]):
+    IWP_column[i]=(ice_water_mc[i,]*length_gridbox_cube)
+
+IWP_cube_list=iris.cube.CubeList(IWP_column)
+IWP=IWP_cube_list.merge()[0]
+IWP=IWP.collapsed(['model_level_number'],iris.analysis.SUM)
+IWP._var_name='IWP'
+IWP.long_name='Ice water path'
+save_cube(IWP)
+
+
+#CLOUD TOP TEMPERATURE   
+cube_l = iris.load(ukl.Obtain_name(path+'/All_time_steps/','m01s00i254'))[0]
+cube_i = iris.load(ukl.Obtain_name(path+'/All_time_steps/','m01s00i012'))[0]
+cloud_mass=cube_l.data[:,:,:,:]+cube_i.data[:,:,:,:]
+cloud_mass[cloud_mass<1e-6]=0
+temp_cloud=temperature.copy()
+temp_cloud_data=temp_cloud.data
+temp_cloud_data[cloud_mass==0]=999
+temp_cloud.data=temp_cloud_data
+temp_cloud_top=temp_cloud.collapsed(['model_level_number'],iris.analysis.MIN)
+
+temp_cloud_top_data=temp_cloud_top.data
+temp_cloud_top_data[temp_cloud_top_data==999]=np.nan
+temp_cloud_top.data=temp_cloud_top_data
+temp_cloud_top._var_name='CTT'
+temp_cloud_top.long_name='Cloud_top_temperature'
+
+save_cube(temp_cloud_top)
+
+
+
+jl.send_email()
+
+#%%
+#plt.figure()
+#plt.contourf(LWP[13,].data,norm = LogNorm(),cmap=plt.cm.Blues)
+#plt.colorbar()
+#plt.show()
+'''
+#%%
 #Obtain mass mixing ratios from netcdf
 particles_mixing_ratio={}
 particles_concentration={}
@@ -111,6 +243,7 @@ for name in mass_mixing_ratio.keys():
     mass_concentration[new_short_name].long_name='mass_concentration'+mass_mixing_ratio[name].long_name[13:-7]
     mass_concentration[new_short_name]._var_name=new_short_name
     save_cube(mass_concentration[new_short_name])
+'''
 '''
 mass_concentration_per_mode={}
 for mode_name in ims.mode_names:
